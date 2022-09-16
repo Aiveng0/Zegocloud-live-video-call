726ac70c449677d1e0e88ecc06b179b2d28714a3
Ivan Tsaryk <48809714+Aiveng0@users.noreply.github.com>
2022-09-16 13:41:19 +0300

    Added wakelock and refactored (#10)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:zegocloud_live_video_call/ui/pages/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
--- lib/utils/zego_express_manager.dart
+++ lib/utils/zego_express_manager.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_final_fields
+
 import 'dart:developer';
 
 import 'package:flutter/cupertino.dart';
