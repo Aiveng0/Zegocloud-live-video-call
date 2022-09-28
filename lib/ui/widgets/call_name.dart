7075408eb58cbf3db49f94d17a1811e80ae251de
Ivan Tsaryk <tsarik197566@gmail.com>
2022-09-28 17:04:08 +0300

    Refactored

--- lib/ui/widgets/call_name.dart
+++ lib/ui/widgets/call_name.dart
@@ -2,24 +2,22 @@ import 'package:flutter/material.dart';
 
 class CallName extends StatelessWidget {
   const CallName({
-    super.key,
-    required this.name,
-  });
+    Key? key,
+    required this.callName,
+  }) : super(key: key);
 
-  final String name;
+  final String callName;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Expanded(
-          child: Text(
-            name,
-            style: const TextStyle(
-              color: Colors.white,
-            ),
-            overflow: TextOverflow.ellipsis,
+        Text(
+          callName,
+          style: const TextStyle(
+            color: Colors.white,
           ),
+          overflow: TextOverflow.ellipsis,
         ),
         const Icon(
           Icons.arrow_right,
