aa7343e4f760a24a93e00f3c53b3fb77424ae47c
Ivan Tsaryk <tsarik197566@gmail.com>
2022-10-12 14:31:03 +0300

    Refactored

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -95,50 +95,47 @@ class _HomePageState extends State<HomePage> {
                   crossAxisAlignment: WrapCrossAlignment.start,
                   spacing: 10,
                   children: [
-                    ElevatedButton(
+                    ActionChip(
                       onPressed: () async {
                         _userIDController.text = '1234';
                         _tokenController.text =
                             '03AAAFgIv2DRgAEDlhM3dvMjZzc294Z2ZwZGcAoGJfBSXdpDdRTJuu4WYcfRwoPyuMQJ/qgrEC35wny+SS0iKyf0uw952DZrl5w1sA7hrunqOaXmD7qf7sdEdRG4jQI3f+tLjHyF70dIS9voneUXhAKhEP8pRAZIMuJTJvSCc5Pnt4AdOwcm6kSsJivUI49bxdH7w0/DWqO90S9omr0a6RqN1ZeD/Rv+rkdgp7i6Nit6A7cxUP8i/VJm7Y550=';
                       },
-                      child: const Text('User 1234 (default)'),
+                      label: const Text('User 1234 (default)'),
                     ),
-                    ElevatedButton(
+                    ActionChip(
                       onPressed: () async {
                         _userIDController.text = '1111';
                         _tokenController.text =
                             '03AAAFgIv2Db4AEHp3ZmswbDhrbWlxMWhrOWgAoLZdgC2GhBcgr2c4wJe9Y4mkwEex1NUvRhguotzAXmGTRC0J9+RYtMdAgDauDQsxtFmLf1fcQwBStBky72tFBjsU6V3eIcEjGac0qn97lh3ICcIcUtFNqdX5y6KfI3kCZ/gcgz7R3MEqvGqdltjZUMTcCtrYo1/uug0YVU3e5qosAltKNC6VTSF8W4ZPVaBk99pjr07ljFTD49P+BNkBQTs=';
                       },
-                      child: const Text('User 1111'),
+                      label: const Text('User 1111'),
                     ),
-                    ElevatedButton(
+                    ActionChip(
                       onPressed: () async {
                         _userIDController.text = '2222';
                         _tokenController.text =
                             '03AAAFgIv2DegAEDQ2aHFjMXl0Y2Y1cmwxdGIAoP0OMgXKUaDBLs4XqRRX6DIHPiTOoKbtwyTi9Y4BO9gU/1WZFOUu4SXF1tLDoAtGG/of9vpxz/I+hafJasO7FLeQHs+EsLYWFUSaDxv/max29EPHoSYFSYAvzE93pc5z5gDBN5OSMgLWotKBooNF7L5PsxjVnedTevjqBQeu/bHhnbYWpj7cv2zwlMFWEKuOCaBlNzHLO1lOMV7OlmktJ5A=';
                       },
-                      child: const Text('User 2222'),
+                      label: const Text('User 2222'),
                     ),
-                    ElevatedButton(
+                    ActionChip(
                       onPressed: () async {
                         _userIDController.text = '3333';
                         _tokenController.text =
                             '03AAAFgIv8o1sAEHE0cHI5MG80b2hvZ3VtaGcAoBAYRod1k063/0fVkzFJLbfT7sur97BB0Zu1YEVKP1BE77b32wAcF74fqEpbZtFIZ0NDRvjpnp7/6m+okcCgpEUmpnuVvLU0WltK3c2OgoEu6eCp33qJMcG45MvIgxkp7gjlWCCbqldKQQMnoYl+V/aP7+C0Gf1w3o+uRd5qPQw+QK2U/x9OE2VbcXeqmyCGqNJwdh/3u4BZ2RBB9zL9DJA=';
                       },
-                      child: const Text('User 3333'),
+                      label: const Text('User 3333'),
                     ),
-                    ElevatedButton(
+                    ActionChip(
                       onPressed: () async {
                         _userIDController.text = '4444';
                         _tokenController.text =
                             '03AAAFgIv8o2cAEGd2YmI2czVtcjU2MXhhemoAoBNQB4fUO4LxQ7daF4IKj8oLEPp17v3ATP1c2sdOxqgpRoxsVMFhRmd9kiZ1Xw6PsHBsdDgmv55i1In+y9TpIykITxjZeoVSilI+TY17JjRCHcoJHm1duQMT66Exls+0XnLeYdkGRAfyyV8gStNoL59BsTFlIvJdsoo+vcEFnlSRSGcc46brfTMA+mB+4ISninMbqOVLkV2fGRkU+uDqMsA=';
                       },
-                      child: const Text('User 4444'),
+                      label: const Text('User 4444'),
                     ),
                   ],
-                ),
-                const Divider(
-                  color: Colors.black87,
                 ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
